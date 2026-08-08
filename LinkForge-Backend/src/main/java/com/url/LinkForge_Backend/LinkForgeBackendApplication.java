4b632a96b964b258a9b89ccba9e44563d1b2e480
Saikiranvalsa <valsasaikiran26@gmail.com>
2026-08-08 12:04:22 +0530

    Inital setup

---
+++ LinkForge-Backend/src/main/java/com/url/LinkForge_Backend/LinkForgeBackendApplication.java
@@ -0,0 +1,13 @@
+package com.url.LinkForge_Backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LinkForgeBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LinkForgeBackendApplication.class, args);
+	}
+
+}
